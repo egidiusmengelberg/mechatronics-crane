b88edc6df351007073450f8aabb2ce6a06f1cec6
Egidius Mengelberg <mengelberg@mengelberg.eu>
2023-01-24 14:17:32 +0100

    config

--- config.h
+++ config.h
@@ -62,72 +62,72 @@
     #define button_position_y_num PC6
 
     //UI BUTTONS
-    #define button_ui_up_ddr DDRF
-    #define button_ui_up_pin PINF
-    #define button_ui_up_port PORTF
-    #define button_ui_up_num PF1
+    #define button_ui_up_ddr DDRH // pin 6
+    #define button_ui_up_pin PINH
+    #define button_ui_up_port PORTH
+    #define button_ui_up_num PH3
 
-    #define button_ui_down_ddr DDRF
-    #define button_ui_down_pin PINF
-    #define button_ui_down_port PORTF
-    #define button_ui_down_num PF2
+    #define button_ui_down_ddr DDRE //pin 5
+    #define button_ui_down_pin PINE
+    #define button_ui_down_port PORTE
+    #define button_ui_down_num PE3
 
-    #define button_ui_select_ddr DDRF
-    #define button_ui_select_pin PINF
-    #define button_ui_select_port PORTF
-    #define button_ui_select_num PF3
+    #define button_ui_select_ddr DDRG //pin 4
+    #define button_ui_select_pin PING
+    #define button_ui_select_port PORTG
+    #define button_ui_select_num PG5
     
-    #define button_ui_start_ddr DDRF
-    #define button_ui_start_pin PINF
-    #define button_ui_start_port PORTF
-    #define button_ui_start_num PF4
+    #define button_ui_start_ddr DDRE // pin 3
+    #define button_ui_start_pin PINE
+    #define button_ui_start_port PORTE
+    #define button_ui_start_num PE5
 
 //OUTPUTS
 
     //RELAYS
-    #define relay_x_ddr DDRC
+    #define relay_x_ddr DDRC //30
     #define relay_x_port PORTC
-    #define relay_x_num PC3
+    #define relay_x_num PC7
 
-    #define relay_y_ddr DDRC
+    #define relay_y_ddr DDRC //32
     #define relay_y_port PORTC
-    #define relay_y_num PC4
+    #define relay_y_num PC5
 
-    #define relay_z_ddr DDRC
-    #define relay_z_port PORTC
-    #define relay_z_num PC5
+    #define relay_z_ddr DDRA //28
+    #define relay_z_port PORTA
+    #define relay_z_num PA6
 
-    #define relay_magnet_ddr DDRC
-    #define relay_magnet_port PORTC
-    #define relay_magnet_num PC6
+    #define relay_magnet_ddr DDRA //26
+    #define relay_magnet_port PORTA
+    #define relay_magnet_num PA4
 
     //LIGHTS
-    #define light_panel_ddr DDRC
-    #define light_panel_port PORTC
-    #define light_panel_num PC6
+    #define light_panel_ddr DDRE //2
+    #define light_panel_port PORTE
+    #define light_panel_num PE4
 
 //INTERFACES
 
     //DISPLAY
-    #define display_din_ddr DDRC
+    #define display_din_ddr DDRC //34
     #define display_din_port PORTC
-    #define display_din_num PC0
+    #define display_din_num PC3
 
-    #define display_clk_ddr DDRC
-    #define display_clk_port PORTC
-    #define display_clk_num PC1
+    #define display_clk_ddr DDRD //38
+    #define display_clk_port PORTD
+    #define display_clk_num PD7
 
-    #define display_cs_ddr DDRC
+    #define display_cs_ddr DDRC // 36
     #define display_cs_port PORTC
-    #define display_cs_num PC2
+    #define display_cs_num PC1
 
     //MOTOR
-    #define motor_en_l_ddr DDRC
-    #define motor_en_l_port PORTC
-    #define motor_en_l_num PC1
+    #define motor_en_l_ddr DDRA // pin 24
+    #define motor_en_l_port PORTA
+    #define motor_en_l_num PA2
 
-    #define motor_en_r_ddr DDRC
-    #define motor_en_r_port PORTC
-    #define motor_en_r_num PC2
+    #define motor_en_r_ddr DDRA // pin 22
+    #define motor_en_r_port PORTA
+    #define motor_en_r_num PA0
 
 #endif
